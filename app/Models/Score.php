7138634d77ac7effa002800cfa4c0e5eeff79cdc
m.zargarov <m.zargarov@framework.team>
2024-12-01 21:14:18 +0400

    Add models

---
+++ app/Models/Score.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Score extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'teacher_id',
+        'test_id',
+        'mark',
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function test(): BelongsTo
+    {
+        return $this->belongsTo(Test::class);
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'middle_name',
+        'birthday',
+        'group_id',
+    ];
+
+    public function user(): MorphOne
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function scores(): HasMany
+    {
+        return $this->hasMany(Score::class);
+    }
+}
