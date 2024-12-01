fad19c6f1e71c6b0ebffa5b2b00eac0b2c367bad
m.zargarov <m.zargarov@framework.team>
2024-12-01 20:31:15 +0400

    Init project

---
+++ database/migrations/2024_11_27_204701_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained('groups');
+            $table->foreignId('subject_id')->constrained('subjects');
+            $table->string('type');
+            $table->integer('semester');
+            $table->timestamps();
+            $table->unique(['group_id', 'subject_id', 'type', 'semester']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tests');
+    }
+};
